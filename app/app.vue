0fd74cb9d9e6b463c7de1b0e74a2c617b8a9890a
Gael Mapwata <gmapwata6@gmail.com>
2025-09-10 15:29:46 +0200

    chore(frontend): init nuxt + vuetify

---
+++ app/app.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-app>
+    <v-main>
+      <v-container class="py-8">
+        <h1 class="text-h5 mb-4">Booking Memo</h1>
+        <v-btn color="primary" prepend-icon="mdi-rocket">Vuetify prêt</v-btn>
+        <NuxtPage />
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script setup lang="ts">
+</script>
---
+++ nuxt.config.ts
@@ -0,0 +1,17 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2025-07-15',
+  devtools: { enabled: true },
+  css: [
+    'vuetify/styles',
+    '@mdi/font/css/materialdesignicons.css'
+  ],
+  build: {
+    transpile: ['vuetify']
+  },
+  vite: {
+    define: {
+      'process.env.DEBUG': false
+    }
+  }
+})
